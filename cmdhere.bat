976a7f86726cb43e9fb6536ff0a2539cdefb1960
phus <phus.lu@gmail.com>
2011-09-07 17:30:32 +0800

    Edited cmdhere.bat via GitHub

--- cmdhere.bat
+++ cmdhere.bat
@@ -1,17 +1,20 @@
-path %~dp0;c:\windows;c:\windows\system32;c:\windows\system32\wbem;%PATH%
 
-set TIP=@shell32.dll,-22022
-reg add "HKEY_CURRENT_USER\Console\%%SystemRoot%%_system32_cmd.exe" /v "ScreenBufferSize" /t REG_DWORD /d 65536100 /f
-reg add "HKEY_CURRENT_USER\Console\%%SystemRoot%%_system32_cmd.exe" /v "WindowSize" /t REG_DWORD /d 2293860 /f
+reg add "HKEY_CURRENT_USER\Console" /v "ScreenBufferSize" /t REG_DWORD /d 65536100 /f
+reg add "HKEY_CURRENT_USER\Console" /v "WindowSize" /t REG_DWORD /d 2293860 /f
+reg add "HKEY_CURRENT_USER\Console" /v "QuickEdit" /t REG_DWORD /d 1 /f
+
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor" /v "DisableUNCCheck" /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor" /v "PathCompletionChar" /t REG_DWORD /d 64 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor" /v "CompletionChar" /t REG_DWORD /d 9 /f
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v "DisableUNCCheck" /t REG_DWORD /d 1 /f
-ver | findstr /r "5\.[01]\." && (
-    reg add "HKEY_CLASSES_ROOT\Folder\shell\DOS" /v "" /t REG_SZ /d "%TIP%" /f
-    reg add "HKEY_CLASSES_ROOT\Folder\shell\DOS\command" /v "" /t REG_SZ /d "%ComSpec% /d" /f
+
+ver | findstr /r "5\." && (
+    reg add "HKEY_CLASSES_ROOT\Folder\shell\DOS" /v "" /t REG_SZ /d "@shell32.dll,-22022" /f
+    reg add "HKEY_CLASSES_ROOT\Folder\shell\DOS\command" /v "" /t REG_SZ /d "cmd.exe /d /s /k ver" /f
 )
-ver | findstr /r "6\.[01]\." && (
-    reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\DOS" /v "" /t REG_SZ /d "%TIP%" /f
-    reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\DOS\command" /v "" /t REG_SZ /d "%ComSpec% /d" /f
+ver | findstr /r "6\." && (
+    reg add "HKEY_CLASSES_ROOT\Folder\shell\runas" /v "" /t REG_SZ /d "@shell32.dll,-22022" /f
+    reg add "HKEY_CLASSES_ROOT\Folder\shell\runas" /v "Icon" /t REG_SZ /d "%windir%\System32\imageres.dll,-78" /f
+    reg add "HKEY_CLASSES_ROOT\Folder\shell\runas\command" /v "" /t REG_SZ /d "cmd.exe /d /s /k ver" /f
 )
+
+pause
